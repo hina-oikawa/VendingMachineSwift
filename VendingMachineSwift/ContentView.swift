546e03b6a88811c716dc1cb9c836066ffabfb046
hina-oikawa <kingu741113@gmail.com>
2021-06-01 15:51:22 +0900

    プロジェクト追加

---
+++ VendingMachineSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VendingMachineSwift
+//
+//  Created by 及川ひな on 2021/06/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
