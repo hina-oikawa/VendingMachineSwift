cedad572c0d8a422a2a7bb87942fc9acaf76377d
hina-oikawa <kingu741113@gmail.com>
2021-06-04 14:26:55 +0900

    ProductViewのファイルを追加

---
+++ VendingMachineSwift/View/ProductView.swift
@@ -0,0 +1,14 @@
+//
+//  ProductView.swift
+//  VendingMachineSwift
+//
+//  Created by 及川ひな on 2021/06/04.
+//
+
+import SwiftUI
+
+struct ProductView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
---
+++ VendingMachineSwift/ViewModel/ProductViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProductViewModel.swift
+//  VendingMachineSwift
+//
+//  Created by 及川ひな on 2021/06/04.
+//
+
+import Foundation
